f314c1acb06dc0ec1a23138906037abae506b043
Scott Rice <scottrice10@gmail.com>
2015-01-27 10:20:44 -0800

    Update nodejs_configuration.rb

--- opsworks_nodejs/libraries/nodejs_configuration.rb
+++ opsworks_nodejs/libraries/nodejs_configuration.rb
@@ -4,11 +4,9 @@ module OpsWorks
       if File.exists?("#{app_root_path}/package.json")
         Chef::Log.info("package.json detected. Running npm #{npm_install_options}.")
         Chef::Log.info(OpsWorks::ShellOut.shellout("sudo su - #{app_config[:user]} -c 'cd #{app_root_path} && npm #{npm_install_options}' 2>&1"))
-      end
-      
-      if File.exists?("#{app_root_path}/bower.json")
         Chef::Log.info("bower.json detected. Running bower install.")
-        Chef::Log.info(OpsWorks::ShellOut.shellout("sudo su - #{app_config[:user]} -c 'cd #{app_root_path} && bower install' 2>&1"))
+        Chef::Log.info(OpsWorks::ShellOut.shellout("npm install -g bower 2>&1"))
+        Chef::Log.info(OpsWorks::ShellOut.shellout("bower install 2>&1"))
       end
     end
   end
